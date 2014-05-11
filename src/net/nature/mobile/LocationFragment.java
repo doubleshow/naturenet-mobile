0a7132a16c6b19dabe9910af4b0baff070af8e70
Tom Yeh <doubleshow@gmail.com>
2014-05-11 11:12:47 -0600

    refactor location/map to fragments

---
+++ src/net/nature/mobile/LocationFragment.java
@@ -0,0 +1,397 @@
+package net.nature.mobile;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+public class LocationFragment extends Fragment implements
+LocationListener,
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
+
+
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
+	private LocationRequest mLocationRequest;
+	private LocationRequest setInterval;
+	// Handle to SharedPreferences for this app
+	SharedPreferences mPrefs;
+
+	// Handle to a SharedPreferences editor
+	SharedPreferences.Editor mEditor;
+	private boolean mUpdatesRequested = true;
+
+	private TextView mConnectionState;
+	private TextView mConnectionStatus;
+	private TextView mLatLng;
+	
+	private LocationListener mLocationListenerActivity;
+
+
+	@Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mLocationListenerActivity = (LocationListener) activity;
+        } catch (ClassCastException e) {
+            //throw new ClassCastException(activity.toString() + " must implement LocationListener");
+        	mLocationListenerActivity = null;
+        }
+    }
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_location, container, false);
+
+
+		// Get handles to the UI view objects
+		mConnectionStatus = (TextView) view.findViewById(R.id.location_connection_status);
+		mConnectionState = (TextView) view.findViewById(R.id.location_connection_state);
+		mLatLng = (TextView) view.findViewById(R.id.location_latlng);
+
+		// Create a new global location parameters object
+		mLocationRequest = LocationRequest.create();
+
+		/*
+		 * Set the update interval
+		 */
+		mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+		// Use high accuracy
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+		// Set the interval ceiling to one minute
+		mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+
+		// Note that location updates are off until the user turns them on
+		mUpdatesRequested = true;
+
+		// Open Shared Preferences
+		mPrefs = getActivity().getSharedPreferences(LocationUtils.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+
+		// Get an editor
+		mEditor = mPrefs.edit();
+
+		/*
+		 * Create a new location client, using the enclosing class to
+		 * handle callbacks.
+		 */
+		mLocationClient = new LocationClient(getActivity(), this, this);
+
+		return view;
+	}
+
+	/*
+	 * Called when the Activity is no longer visible at all.
+	 * Stop updates and disconnect.
+	 */
+	@Override
+	public void onStop() {
+
+		// If the client is connected
+		if (mLocationClient.isConnected()) {
+			stopPeriodicUpdates();
+		}
+
+		// After disconnect() is called, the client is considered "dead".
+		mLocationClient.disconnect();
+
+		super.onStop();
+	}
+	/*
+	 * Called when the Activity is going into the background.
+	 * Parts of the UI may be visible, but the Activity is inactive.
+	 */
+	@Override
+	public void onPause() {
+
+		// Save the current setting for updates
+		mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, mUpdatesRequested);
+		mEditor.commit();
+
+		super.onPause();
+	}
+
+	/*
+	 * Called when the Activity is restarted, even before it becomes visible.
+	 */
+	@Override
+	public void onStart() {
+
+		super.onStart();
+
+		/*
+		 * Connect the client. Don't re-start any requests here;
+		 * instead, wait for onResume()
+		 */
+		mLocationClient.connect();
+
+	}
+	/*
+	 * Called when the system detects that this Activity is now visible.
+	 */
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		// If the app already has a setting for getting location updates, get it
+		if (mPrefs.contains(LocationUtils.KEY_UPDATES_REQUESTED)) {
+			mUpdatesRequested = mPrefs.getBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
+
+			// Otherwise, turn off location updates until requested
+		} else {
+			mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
+			mEditor.commit();
+		}
+
+	}	
+
+	/**
+	 * Verify that Google Play services is available before making a request.
+	 *
+	 * @return true if Google Play services is available, otherwise false
+	 */
+	private boolean servicesConnected() {
+
+		// Check that Google Play services is available
+		int resultCode =
+				GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+			// In debug mode, log the status
+			Log.d(LocationUtils.APPTAG, getString(R.string.play_services_available));
+
+			// Continue
+			return true;
+			// Google Play services was not available for some reason
+		} else {
+			// Display an error dialog
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(), 0);
+			if (dialog != null) {
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(dialog);
+				errorFragment.show(getFragmentManager(), LocationUtils.APPTAG);
+			}
+			return false;
+		}
+	}
+
+
+	/**
+	 * Invoked by the "Start Updates" button
+	 * Sends a request to start location updates
+	 *
+	 * @param v The view object associated with this method, in this case a Button.
+	 */
+	public void startUpdates(View v) {
+		mUpdatesRequested = true;
+
+		if (servicesConnected()) {
+			startPeriodicUpdates();
+		}
+	}
+
+	/**
+	 * Invoked by the "Stop Updates" button
+	 * Sends a request to remove location updates
+	 * request them.
+	 *
+	 * @param v The view object associated with this method, in this case a Button.
+	 */
+	public void stopUpdates(View v) {
+		mUpdatesRequested = false;
+
+		if (servicesConnected()) {
+			stopPeriodicUpdates();
+		}
+	}
+
+	/*
+	 * Called by Location Services when the request to connect the
+	 * client finishes successfully. At this point, you can
+	 * request the current location or start periodic updates
+	 */
+	@Override
+	public void onConnected(Bundle bundle) {
+		mConnectionStatus.setText(R.string.connected);
+
+		if (mLocationClient != null && mLocationClient.getLastLocation() != null && mLocationListenerActivity != null){
+			mLocationListenerActivity.onLocationChanged(mLocationClient.getLastLocation());
+		}	
+		
+		if (mUpdatesRequested) {
+			startPeriodicUpdates();
+		}
+	}
+
+	/*
+	 * Called by Location Services if the connection to the
+	 * location client drops because of an error.
+	 */
+	@Override
+	public void onDisconnected() {
+		mConnectionStatus.setText(R.string.disconnected);
+	}
+
+	/*
+	 * Called by Location Services if the attempt to
+	 * Location Services fails.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+
+		/*
+		 * Google Play services can resolve some errors it detects.
+		 * If the error has a resolution, try sending an Intent to
+		 * start a Google Play services activity that can resolve
+		 * error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(
+						getActivity(),
+						LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+
+			} catch (IntentSender.SendIntentException e) {
+
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+
+			// If no resolution is available, display a dialog to the user with the error.
+			showErrorDialog(connectionResult.getErrorCode());
+		}
+	}
+
+	/**
+	 * Show a dialog returned by Google Play services for the
+	 * connection error code
+	 *
+	 * @param errorCode An error code returned from onConnectionFailed
+	 */
+	private void showErrorDialog(int errorCode) {
+
+		// Get the error dialog from Google Play services
+		Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+				errorCode,
+				getActivity(),
+				LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+		// If Google Play services can provide an error dialog
+		if (errorDialog != null) {
+
+			// Create a new DialogFragment in which to show the error dialog
+			ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+
+			// Set the dialog in the DialogFragment
+			errorFragment.setDialog(errorDialog);
+
+			// Show the error dialog in the DialogFragment
+			errorFragment.show(getFragmentManager(), LocationUtils.APPTAG);
+		}
+	}
+
+	/**
+	 * Report location updates to the UI.
+	 *
+	 * @param location The updated location.
+	 */
+	@Override
+	public void onLocationChanged(Location location) {
+
+		// Report to the UI that the location was updated
+		mConnectionStatus.setText(R.string.location_updated);
+
+		// In the UI, set the latitude and longitude to the value received
+		mLatLng.setText(LocationUtils.getLatLng(getActivity(), location));
+						
+		if (mLocationListenerActivity != null)
+			mLocationListenerActivity.onLocationChanged(location);
+	}
+
+	/**
+	 * In response to a request to start updates, send a request
+	 * to Location Services
+	 */
+	private void startPeriodicUpdates() {
+
+		mLocationClient.requestLocationUpdates(mLocationRequest, this);
+		mConnectionState.setText(R.string.location_requested);
+	}
+
+	/**
+	 * In response to a request to stop updates, send a request to
+	 * Location Services
+	 */
+	private void stopPeriodicUpdates() {
+		mLocationClient.removeLocationUpdates(this);
+		mConnectionState.setText(R.string.location_updates_stopped);
+	}
+
+
+
+
+	/**
+	 * Define a DialogFragment to display the error dialog generated in
+	 * showErrorDialog.
+	 */
+	public static class ErrorDialogFragment extends android.app.DialogFragment {
+
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+
+		/**
+		 * Default constructor. Sets the dialog field to null
+		 */
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+
+		/**
+		 * Set the dialog to display
+		 *
+		 * @param dialog An error dialog
+		 */
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+
+		/*
+		 * This method must return a Dialog to the DialogFragment.
+		 */
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
+
+}
---
+++ src/net/nature/mobile/MapFragment.java
@@ -0,0 +1,127 @@
+package net.nature.mobile;
+
+import com.nutiteq.MapView;
+import com.nutiteq.components.Color;
+import com.nutiteq.components.Components;
+import com.nutiteq.components.MapPos;
+import com.nutiteq.components.Range;
+import com.nutiteq.geometry.Marker;
+import com.nutiteq.projections.EPSG3857;
+import com.nutiteq.rasterdatasources.HTTPRasterDataSource;
+import com.nutiteq.rasterdatasources.RasterDataSource;
+import com.nutiteq.rasterlayers.RasterLayer;
+import com.nutiteq.style.MarkerStyle;
+import com.nutiteq.ui.DefaultLabel;
+import com.nutiteq.ui.Label;
+import com.nutiteq.utils.UnscaledBitmapLoader;
+import com.nutiteq.vectorlayers.MarkerLayer;
+
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+
+public class MapFragment extends Fragment {
+
+	private MapView mMapView;
+	private MarkerLayer mMarkerLayer;
+	private View mButtonCurrentPosition;	
+	private Location mCurrentLocation;
+	private Marker mMarker;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+		mMapView = (MapView)  view.findViewById(R.id.map_view);
+		mButtonCurrentPosition = (View)  view.findViewById(R.id.map_button_mylocation);
+
+		// define new configuration holder object
+		mMapView.setComponents(new Components());
+
+		// Define base layer. Here we use MapQuest open tiles which are free to use
+		// Almost all online maps use EPSG3857 projection.
+		// We use online data source for the tiles and the URL is given as template. 
+		RasterDataSource dataSource = new HTTPRasterDataSource(new EPSG3857(), 0, 19, "http://otile1.mqcdn.com/tiles/1.0.0/osm/{zoom}/{x}/{y}.png");
+		RasterLayer mapLayer = new RasterLayer(dataSource, 0);
+		mMapView.getLayers().setBaseLayer(mapLayer);
+
+		// do not allow to rotate map with two-finger touch. There is no rotation constraints by degrees, just on/off
+		mMapView.getConstraints().setRotatable(false);
+
+		// Set allowed zoom range and bounding box. 
+		// Bounding box must be in layer projection units, so conversion is needed.
+		mMapView.getConstraints().setZoomRange(new Range(12, 19));
+
+		mMapView.setMapRotation(0f);
+		// zoom - 0 = world, like on most web maps
+		mMapView.setZoom(18.0f);
+
+
+		//MapEventListener mapListener = new MapEventListener(this);
+		//mMapView.getOptions().setMapListener(mapListener);
+
+		mMarkerLayer = new MarkerLayer(mapLayer.getProjection());
+		mMapView.getLayers().addLayer(mMarkerLayer);
+
+		mButtonCurrentPosition.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if (mCurrentLocation != null){
+					double latitude = mCurrentLocation.getLatitude();
+					double longitude = mCurrentLocation.getLongitude();
+					mMapView.setFocusPoint(mMapView.getLayers().getBaseLayer().getProjection().fromWgs84(longitude, latitude));
+				}
+			}        	
+		});
+		return view;		
+	}
+
+	@Override
+	public void onStop() {
+		//Stop the map - mandatory to avoid problems with app restart
+		mMapView.stopMapping();
+		super.onStop();
+	}
+
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		//Start the map - mandatory
+		mMapView.startMapping();
+	}
+
+	public void setCurrentLocation(Location location) {
+		double latitude = location.getLatitude();
+		double longitude = location.getLongitude();
+		if (mCurrentLocation == null){
+			// if it's the first time acquiring a location, focus the map to the location
+			mMapView.setFocusPoint(mMapView.getLayers().getBaseLayer().getProjection().fromWgs84(longitude, latitude));
+		}
+		mCurrentLocation = location;
+		MapPos markerLocation = mMapView.getLayers().getBaseLayer().getProjection().fromWgs84(longitude, latitude);
+		if (mMarker == null){
+			// define marker style (image, size, color)
+			Bitmap pointMarker = UnscaledBitmapLoader.decodeResource(getResources(), R.drawable.cur_position);
+			MarkerStyle markerStyle = MarkerStyle.builder().setBitmap(pointMarker).setSize(1.0f).setColor(Color.WHITE).build();
+			//			// define label what is shown when you click on marker
+			Label markerLabel = new DefaultLabel("You are here");//, "Here is a marker");
+			//
+			mMarker = new Marker(markerLocation, markerLabel, markerStyle, mMarkerLayer);
+			mMarkerLayer.clear();		
+			mMarkerLayer.add(new Marker(markerLocation, markerLabel, markerStyle, mMarkerLayer));
+		}
+
+		mMarker.setMapPos(markerLocation);		
+		mMarkerLayer.clear();
+		mMarkerLayer.add(mMarker);
+		mMapView.invalidate();		
+	}
+}
